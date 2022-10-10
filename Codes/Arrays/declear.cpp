0c63860794855346b9b979f34dadc8b804df9f33
atharvapawar181 <70618288+atharvapawar181@users.noreply.github.com>
2022-10-10 19:12:29 +0530

    Update declear.cpp

--- Codes/Arrays/declear.cpp
+++ Codes/Arrays/declear.cpp
@@ -5,17 +5,4 @@ int main(){
   int n;
   cin>>n;
 
-  int array[n];
-  for (int i = 0; i < n; i++)
-  {
-    cin>>array[i];
-  }
-
-  for (int i = 0; i < n; i++)
-  {
-    cout<<array[i]<<" ";
-  }
-  return 0;
-  
-
-}
+ 
