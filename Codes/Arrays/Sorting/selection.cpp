6c1fd6875d7e99fef90612f900f27df1014f2ea4
atharvapawar181 <70618288+atharvapawar181@users.noreply.github.com>
2022-10-10 19:11:03 +0530

    Update selection.cpp

--- Codes/Arrays/Sorting/selection.cpp
+++ Codes/Arrays/Sorting/selection.cpp
@@ -11,17 +11,7 @@ int main(){
     cin>>array[i];
   }
 
-  for(int i=0; i<n-1; i++){
-    for (int j=i+1; j<n; j++)
-    {
-      if (array[j]<array[i])
-      {
-        int temp = array[j];
-        array[j] = array[i];
-        array[i] = temp;
-      } 
-    }  
-  }
+  
   for (int i = 0; i < n; i++)
   {
     cout<<array[i]<<" ";
