efff584c6ff79422f76f41c7b082cc380aaac25b
atharvapawar181 <70618288+atharvapawar181@users.noreply.github.com>
2022-10-10 19:09:35 +0530

    Update primeab.cpp

--- Codes/Break  and Continue/primeab.cpp
+++ Codes/Break  and Continue/primeab.cpp
@@ -11,16 +11,6 @@ int main(){
   for (int num = a; num <= b; num++)
   {
     int i;
-    for ( i=2; i<num ; i++)
-    {
-      if(num%i==0){
-        break;
-      }
-    }
-    if (i==num)
-    {
-      cout<<num<<endl;
-    }
-  }
+   
   
 }
