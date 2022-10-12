210b493582e63b4770324cdde2af557e38b5b5db
Viraj Tilekar <67374433+VirajTilekar@users.noreply.github.com>
2022-10-12 15:27:11 +0530

    Update selection.cpp

--- Codes/Arrays/Sorting/selection.cpp
+++ Codes/Arrays/Sorting/selection.cpp
@@ -11,7 +11,17 @@ int main(){
     cin>>array[i];
   }
 
-  
+  for(int i=0; i<n-1; i++){
+    for (int j=i+1; j<n; j++)
+    {
+      if (array[j]<array[i])
+      {
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+      } 
+    }  
+  }
   for (int i = 0; i < n; i++)
   {
     cout<<array[i]<<" ";
